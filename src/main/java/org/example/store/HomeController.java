6f4e63798879e961eea1d43db10a8d1fae4468e2
ZadenMubarak <zaden.ngobeni@gmail.com>
2025-06-09 22:46:42 +0200

    White label error fixed

--- src/main/java/org/example/store/HomeController.java
+++ src/main/java/org/example/store/HomeController.java
@@ -5,21 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-
     @RequestMapping("/")
     public String index(){
         //send request to root of the site
+        System.out.println("RUNNING HOME CTRLER");
         return "index.html";
     }
 }
-/*
-*
-*  .   ____          _            __ _ _
- /\\ / ___'_ __ _ _(_)_ __  __ _ \ \ \ \
-( ( )\___ | '_ | '_| | '_ \/ _` | \ \ \ \
- \\/  ___)| |_)| | | | | || (_| |  ) ) ) )
-  '  |____| .__|_| |_|_| |_\__, | / / / /
- =========|_|==============|___/=/_/_/_/
 
-
-*/
