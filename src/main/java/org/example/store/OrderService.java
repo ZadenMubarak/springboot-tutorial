6040c69bee46e43ffd7d500b0c34e35eb26ac222
ZadenMubarak <zaden.ngobeni@gmail.com>
2025-06-15 00:42:16 +0200

    converted dependencies to using Interfaces

--- src/main/java/org/example/store/OrderService.java
+++ src/main/java/org/example/store/OrderService.java
@@ -2,10 +2,17 @@ package org.example.store;
 
 public class OrderService {
 
-    public void placeOrder(){
-        var paymentServive = new StripePaymentService();
+    PaymentService paymentService;
 
+    public OrderService(PaymentService paymentService){
+        this.paymentService = paymentService;
+    }
+
+    public void placeOrder(){
+
+        var paymentServive = new StripePaymentService();
         paymentServive.processPayment(10);
+
     }
 
     public void cancelOrder(){}
