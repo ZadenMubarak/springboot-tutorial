bff7af4dd476057e9fefbb48a790887e028831a3
ZadenMubarak <zaden.ngobeni@gmail.com>
2025-06-19 14:38:51 +0200

    adding new payment service

---
+++ src/main/java/org/example/store/PaypalPaymentService.java
@@ -0,0 +1,9 @@
+package org.example.store;
+
+public class PaypalPaymentService {
+
+    public void processPayment(double amount){
+        System.out.println("Paypal");
+        System.out.println("Amount:"+amount);
+    }
+}
