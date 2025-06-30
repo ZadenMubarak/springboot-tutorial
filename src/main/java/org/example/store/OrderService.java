bfb612b1bc5a3c44e5618dd5463fa705f9962a90
ZadenMubarak <zaden.ngobeni@gmail.com>
2025-06-30 02:15:23 +0200

    begining with IOC

--- src/main/java/org/example/store/OrderService.java
+++ src/main/java/org/example/store/OrderService.java
@@ -4,15 +4,21 @@ public class OrderService {
 
     PaymentService paymentService;
 
-    public OrderService(PaymentService paymentService){
+//    public OrderService(PaymentService paymentService){
+//        this.paymentService = paymentService;
+//    }
+
+    public void setPaymentSevice(PaymentService paymentService){
         this.paymentService = paymentService;
     }
 
-    public void placeOrder(){
+    public PaymentService getPaymentService(){
+        return this.paymentService;
+    }
 
+    public void placeOrder(){
         var paymentServive = new StripePaymentService();
         paymentServive.processPayment(10);
-        
     }
 
     public void cancelOrder(){}
--- src/main/java/org/example/store/PaypalPaymentService.java
+++ src/main/java/org/example/store/PaypalPaymentService.java
@@ -1,9 +1,10 @@
 package org.example.store;
 
-public class PaypalPaymentService {
+public class PaypalPaymentService implements PaymentService {
 
+    @Override
     public void processPayment(double amount){
-        System.out.println("Paypal");
+        System.out.println("PAYPAL");
         System.out.println("Amount:"+amount);
     }
 }
